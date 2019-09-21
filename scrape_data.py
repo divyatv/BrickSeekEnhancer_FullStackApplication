5dd8b5066329a28bf8aef9212fef051cce36a62d
divya tv <divya.tv@gmail.com>
2019-09-21 18:04:17 -0400

    Saturday evening

--- scrape_data.py
+++ scrape_data.py
@@ -1,3 +1,4 @@
+#from __future__ import division, unicode_literalsi
 import sqlalchemy
 from bs4 import BeautifulSoup
 from sqlalchemy import Column, Integer, String, Float
@@ -5,12 +6,98 @@ from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine, inspect
 import connectDB
-
-# Get zip codes from the city_zip_table
-sql='SELECT zip FROM city_zip_data LIMIT 10'
-zip=connectDB.connect_db(sql)
-
-# Loop through all the zip codes and then build the url
 import re
-for item in zip:
- print (re.sub("\'|\,|\(|\)", "", str(item)))
+import os 
+import codecs
+import pandas as pd
+
+
+def scrape_endpoint(sku):
+    # Get zip codes from the city_zip_table
+    ## select zip from city_zip_data where it matches the SKU provided- change later
+    sql='SELECT zip FROM city_zip_data LIMIT 3'
+    zip_code=connectDB.connect_db(sql)
+
+    # Loop through all the zip codes and then build the url 
+    zip_codes=[]
+    for item in zip_code:
+        zip_codes.append(re.sub("\'|\,|\(|\)", "", str(item)))
+    ##return(zip_codes)
+    for item in zip_codes:
+        command= 'curl --data ' + '"sku=' + str(sku) + '&zip=' + item + \
+                  '" https://brickseek.com/walmart-inventory-checker? -o html'+ item +'.html'
+        os.system(command) 
+    return(zip_codes)    
+    
+
+def read_from_html(htmlfile_name):        
+    filepath = os.path.join(htmlfile_name)
+    with open(filepath, encoding='utf-8') as file:
+        read_html_content = file.read()
+        return (read_html_content)
+
+def scrape_html_pages(zip_code):
+    soup = BeautifulSoup(read_from_html('html'+ zip_code +'.html'), 'html.parser')
+    all_data = soup.find('div', class_="table inventory-checker-table inventory-checker-table--store-availability-price inventory-checker-table--columns-3")
+    # .find('span', class_="table__cell-price-discount")
+    price_value=all_data.find_all('span', class_='table__cell-price-discount')
+    #print(price_value)  
+    discount=[ item.text for item in price_value ]
+    #print("divya", discount)
+    address_ex = [item.text.split('\n')[1] for item in all_data.find_all('address', class_='address') ]
+    #print("address", address_ex)
+    quantity=[ indi.text for indi in all_data.find_all('span', class_='availability-status-indicator__text') ]
+    #print("quantity", quantity)
+    pages_dict = [ {"sku": sku, "zip-code:":  address_ex[i].split()[-1], "price-off": discount[i], \
+                    "store-address" : ' '.join(address_ex[i].split()[:-1]), \
+                    "quantity": quantity[i] } for i in range(len(discount)) ]
+    #for page in pages_dict:
+    #    print(page) 
+    return pages_dict    
+
+def write_to_db(zip_codes):
+
+    page_dict_list = []
+    for item in  [scrape_html_pages(item) for  item in zip_codes]:
+        page_dict_list = page_dict_list + item
+    
+    for i in page_dict_list:
+        print("iiiii",  i)
+
+    # drop city_zip_data if it exist already
+    rds_connection_string = "bedlgjelgbrcba:62edbf5e39edf1ea129a38a5766d7354579374a6db487103a421c76fd47d78c3@ec2-184-73-232-93.compute-1.amazonaws.com:5432/dd4i4baf4sjibo"
+    engine = create_engine(f'postgresql://{rds_connection_string}')
+    engine.table_names()
+    market_scraped_data = 'market_scraped_data'
+    connection = engine.raw_connection()
+    cursor = connection.cursor()
+    command = "DROP TABLE IF EXISTS {};".format(market_scraped_data)
+    cursor.execute(command)
+    connection.commit()
+    cursor.close()
+        
+    pages_data_df = pd.DataFrame.from_dict(page_dict_list)
+    pages_data_df.head(10)
+    pages_data_df.to_sql('market_scraped_data', engine, dtype={col_name: sqlalchemy.types.VARCHAR for col_name in pages_data_df})
+    pd.read_sql_query('select * from market_scraped_data', con=engine).head()
+    # print(page_dict_list)    
+
+
+if __name__ == '__main__' :
+    sku = 625412161 
+    zip_codes=scrape_endpoint(sku)
+    #print(zip_codes)
+    
+    write_to_db(zip_codes)
+    # for item in zip_codes:
+    #     s.remove('html'+ item +'.html')
+
+    for item in zip_codes:
+        file = 'html' + item + '.html'
+        if os.path.exists(file):
+            os.remove(file)
+
+           
+
+     
+    
