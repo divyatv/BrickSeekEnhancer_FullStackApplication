add3cc32f5da894675b36a66ff67a23ad36cabdb
divya tv <divya.tv@gmail.com>
2019-09-24 18:29:10 -0400

    adding latest files

--- Flask_Module_for_Scraped_Data/app.py
+++ Flask_Module_for_Scraped_Data/app.py
@@ -15,6 +15,8 @@ from sqlalchemy import create_engine, inspect
 import connectDB
 import pandas as pd
 
+from flask import json
+
 #app = Flask(__name__)
 app = Flask(__name__, static_url_path='/static')
 
@@ -58,19 +60,32 @@ def table():
 @app.route("/plots")
 def plots():
     """Return entire database entries fot a particular SKU""" 
-    sql='SELECT * FROM market_scraped_data'
-    entire_table=connectDB.connect_db(sql)
+    t = (' 201326711 ',)
 
-    df = pd.DataFrame(entire_table, columns =['priceoff', 'quantity', 'sku', 'storeaddress', 'zipcode']) 
-    #df =df.reset_index(drop=True, inplace=True)
-    data_dict = df.T.to_dict().values()
+    rds_connection_string = "bedlgjelgbrcba:62edbf5e39edf1ea129a38a5766d7354579374a6db487103a421c76fd47d78c3@ec2-184-73-232-93.compute-1.amazonaws.com:5432/dd4i4baf4sjibo"
+    engine = create_engine(f'postgresql://{rds_connection_string}')
+    #engine.table_names()
+    connection = engine.raw_connection()
+    cursor = connection.cursor()
+    entire_table=engine.execute('SELECT priceoff, zipcode, quantity FROM market_scraped_data WHERE sku= %s', t)
+    cursor.close()
 
-   
-    return jsonify(data_dict)
+    df = pd.DataFrame(entire_table, columns =['priceoff', 'zipcode', 'quantity'])
 
+    zipc = df['zipcode'].tolist()
+    valuesc=df['priceoff'].tolist()
+    labels=[]
+    values=[]
+    legend = 'Location'
+    for item in zipc:
+        labels.append(str(item))
+    for item in valuesc:
+        values.append(str(item).replace("% off", ""))
+      
+    return render_template('Chart.html', values=values, labels=labels, legend=legend)
     # Return a list of df
     #return jsonify(data_dict) 
-    return jsonify(list(data_dict))   
+    #return jsonify(list(data_dict))   
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
