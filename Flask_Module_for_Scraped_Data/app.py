dc66b3e695ec3e6e5b276f1b76b808dabf86b693
divya tv <divya.tv@gmail.com>
2019-09-30 12:27:37 -0400

    pushing latest

--- Flask_Module_for_Scraped_Data/app.py
+++ Flask_Module_for_Scraped_Data/app.py
@@ -44,7 +44,7 @@ def home():
 def count_result(user_sku):
    import sys
    print(user_sku,file=sys.stderr)
-   mysqlQuery="SELECT quantity, COUNT(*) FROM market_scraped_data where sku=' "+str(user_sku)+" ' GROUP BY quantity"
+   mysqlQuery="SELECT quantity, COUNT(*) FROM market_scraped_data where sku='"+str(user_sku)+"' GROUP BY quantity"
    data = connectDB.connect_db(mysqlQuery)
    myresult = []
    for result in data:
@@ -72,102 +72,85 @@ def table():
 
     return render_template("table_display.html")
 
-##############################################################################################################
+############Plot the map view#############################################################################################
 
 @app.route('/scrape', methods=['GET','POST'])
 def scrapedmap():
     return render_template("maps_display.html", jsonvalue= json.dumps(scrapebrickseekfinal.scrape_all()))
 
 ##############################################################################################################
-@app.route("/stockstatus/<user_sku>/<user_upc>")
-def stockstatus(user_sku, user_upc):
+@app.route("/stockstatus")
+def stockstatus():
     """Return the page with plot of availability for the sku selected"""
-
-    #sku = (' sku ',)
-    #user_sku='201326711'
-    import sys
-    print(user_sku,file=sys.stderr)
-
     
-    mysqlQuery="SELECT quantity, COUNT(*) FROM walmart_scraped_data where SKU='"+str(user_sku)+"' GROUP BY quantity"
-    entire_table = connectDB.connect_db(mysqlQuery)
+    mysqlQuery_walmart="SELECT quantity, COUNT(*) FROM walmart_scraped_data where SKU='329264833' GROUP BY quantity"
+    entire_tablew = connectDB.connect_db(mysqlQuery_walmart)
 
-    # connection = engine.raw_connection()
-    # cursor = connection.cursor()
-    # #sql="SELECT zipcode, discount, COUNT(*) FROM market_scraped_data where sku=' "+str(user_sku)+" ' GROUP BY discount"
-    # #entire_table=engine.execute("SELECT quantity, COUNT(*) FROM market_scraped_data WHERE sku= ' 201326711 ' GROUP BY quantity")
-    # entire_table=engine.execute("SELECT quantity, COUNT(*) FROM market_scraped_data where sku=' "+str(user_sku)+" ' GROUP BY quantity")
-    # cursor.close()
-
-    df = pd.DataFrame(entire_table, columns =['quantity', 'count']) 
+    dfw = pd.DataFrame(entire_tablew, columns =['quantity', 'count']) 
     # data_list = df.values.tolist()
 
-    data = [
+    dataw = [
         go.Bar(
-            x=df['quantity'], # assign x as the dataframe column 'x'
-            y=df['count']
+            x=dfw['quantity'], # assign x as the dataframe column 'x'
+            y=dfw['count'],
+            name="Walmart"
         )
     ]
 
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSONW = json.dumps(dataw, cls=plotly.utils.PlotlyJSONEncoder)
 
-    import sys
-    print(user_upc,file=sys.stderr)
+    # import sys
+    # print(user_upc,file=sys.stderr)
 
-    mysqlQuery="SELECT quantity, COUNT(*) FROM target_scraped_data where upc='"+str(user_upc)+"' GROUP BY quantity"
-    entire_table = connectDB.connect_target_db(mysqlQuery)
+    mysqlQuery_target="SELECT quantity, COUNT(*) FROM target_scraped_data where upc='190198429612' GROUP BY quantity"
+    entire_tablet = connectDB.connect_target_db(mysqlQuery_target)
 
-    # connection = engine.raw_connection()
-    # cursor = connection.cursor()
-    # #sql="SELECT zipcode, discount, COUNT(*) FROM market_scraped_data where sku=' "+str(user_sku)+" ' GROUP BY discount"
-    # #entire_table=engine.execute("SELECT quantity, COUNT(*) FROM market_scraped_data WHERE sku= ' 201326711 ' GROUP BY quantity")
-    # entire_table=engine.execute("SELECT quantity, COUNT(*) FROM market_scraped_data where sku=' "+str(user_sku)+" ' GROUP BY quantity")
-    # cursor.close()
-
-    df1 = pd.DataFrame(entire_table, columns =['quantity', 'count']) 
+    dft = pd.DataFrame(entire_tablet, columns =['quantity', 'count']) 
     # data_list = df.values.tolist()
 
-    data1 = [
+    datat = [
         go.Bar(
-            x=df1['quantity'], # assign x as the dataframe column 'x'
-            y=df1['count']
-        )
+            x=dft['quantity'], # assign x as the dataframe column 'x'
+            y=dft['count'],
+            name="Target"
+            )
     ]
        
-    graphJSON1 = json.dumps(data1, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSONT = json.dumps(datat, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return render_template("lineplot.html", plot_war=graphJSON, plot_tar=graphJSON1)
-
+    return render_template("barplot.html", plot_war=graphJSONW, plot_tar=graphJSONT)
 
 
 ############################################################################################################
-@app.route("/plouuts/<sku>")
-def plots(sku):
-    """Return entire database entries fot a particular SKU""" 
-    sku = (' sku ',)
-  
+# @app.route("/plots")
+# def plots():
+#     """Plot availablity of the product in Walart vs Target""" 
+#     ##### Keeping this piece of code for future enhancements#####
+#     # sku = (' sku ',)
+#     # rds_connection_string = "end string here"
+#     # engine = create_engine(f'postgresql://{rds_connection_string}')
+#     # #engine.table_names()
+#     # connection = engine.raw_connection()
+#     # cursor = connection.cursor()
+#     # entire_table=engine.execute('SELECT priceoff, zipcode, quantity FROM target_scraped_data WHERE upc= %s', sku)
+#     # cursor.close()
+#     mysqlQuery="SELECT quantity, COUNT(*) FROM target_scraped_data where upc='190198429612' GROUP BY quantity"
+#     entire_table = connectDB.connect_target_db(mysqlQuery)
 
-    rds_connection_string = "bedlgjelgbrcba:62edbf5e39edf1ea129a38a5766d7354579374a6db487103a421c76fd47d78c3@ec2-184-73-232-93.compute-1.amazonaws.com:5432/dd4i4baf4sjibo"
-    engine = create_engine(f'postgresql://{rds_connection_string}')
-    #engine.table_names()
-    connection = engine.raw_connection()
-    cursor = connection.cursor()
-    entire_table=engine.execute('SELECT priceoff, zipcode, quantity FROM market_scraped_data WHERE sku= %s', sku)
-    cursor.close()
+#     df = pd.DataFrame(entire_table, columns =['quantity', 'count'])
 
-    df = pd.DataFrame(entire_table, columns =['priceoff', 'zipcode', 'quantity'])
-
-    zipc = df['zipcode'].tolist()
-    valuesc=df['priceoff'].tolist()
-    labels=[]
-    values=[]
-    legend = 'Location'
-    for item in zipc:
-        labels.append(str(item))
-    for item in valuesc:
-        values.append(str(item).replace("% off", ""))
+#     #zipc = df['zipcode'].tolist()
+#     quantity = df['quantity'].tolist()
+#     valuesc=df['count'].tolist()
+#     labels=[]
+#     values=[]
+#     legend = 'Location'
+#     for item in quantity:
+#         labels.append(str(item))
+#     for item in valuesc:
+#         values.append(str(item).replace("% off", ""))
       
-    return render_template('Chart.html', values=values, labels=labels, legend=legend)
+#     return render_template('Chart.html', values=values, labels=labels, legend=legend)
 ############################################################################################## 
 
 ### Running main function
