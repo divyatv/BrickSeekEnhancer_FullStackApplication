05210c9a2ecd94427e190f263e6cfcc47d83dbe4
divya tv <divya.tv@gmail.com>
2019-10-01 19:34:34 -0400

    removing all files

--- Flask_Module_for_Scraped_Data_Main_Application/config.py
+++
@@ -1,2 +0,0 @@
-# Enter your API key
-gkey = "AIzaSyDeMcxfAnuJB0TRF02BFDHVL_Wa5mfGMpc"
--- Flask_Module_for_Scraped_Data_Main_Application/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoibWtkaW5oOTQiLCJhIjoiY2swY3dxeGMyMDFlNTNtcG01NjJ1MXY4MCJ9.BJBWJZjRxMgaSLo6BHk2DQ";
