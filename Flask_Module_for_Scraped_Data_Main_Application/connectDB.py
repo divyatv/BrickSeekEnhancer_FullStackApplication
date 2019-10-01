1ee17b60d9d68986a3b33860087bff0477b2d995
Divya TV <divya.tv@gmail.com>
2019-09-30 22:07:59 -0400

    Update connectDB.py
    MASKING THE PASSWORD  - let us know if you need it.

--- Flask_Module_for_Scraped_Data_Main_Application/connectDB.py
+++ Flask_Module_for_Scraped_Data_Main_Application/connectDB.py
@@ -6,13 +6,13 @@ from sqlalchemy import create_engine, inspect
 
 #### Function to conect to walmart inventory and fetch data.
 def connect_db(sql):
-    rds_connection_string = "bedlgjelgbrcba:62edbf5e39edf1ea129a38a5766d7354579374a6db487103a421c76fd47d78c3@ec2-184-73-232-93.compute-1.amazonaws.com:5432/dd4i4baf4sjibo"
+    rds_connection_string = "bedlgjelgbrcba:HIDING THE PASSWORD@ec2-184-73-232-93.compute-1.amazonaws.com:5432/dd4i4baf4sjibo"
     engine = create_engine(f'postgresql://{rds_connection_string}')
     return engine.execute(sql).fetchall()
 
 #### Function to connect to target inventory and fetch data.
 def connect_target_db(sql):
-    rds_connection_string = "xsexllfzorcrvb:c19fdb9e7f8b1d787fd0aa79c53c274bd6b25e57c89b6d0981f8b6ad293a5c80@ec2-184-73-232-93.compute-1.amazonaws.com:5432/dbvr413t8b1dv6"
+    rds_connection_string = "xsexllfzorcrvb:HIDING THE PASSWORD@ec2-184-73-232-93.compute-1.amazonaws.com:5432/dbvr413t8b1dv6"
     engine = create_engine(f'postgresql://{rds_connection_string}')
     return engine.execute(sql).fetchall()
       
