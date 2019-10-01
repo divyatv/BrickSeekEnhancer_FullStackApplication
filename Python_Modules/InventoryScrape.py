225ff2fa39c18386c12319dd0f8f4f6d397ed205
Divya TV <divya.tv@gmail.com>
2019-09-30 22:12:46 -0400

    Update InventoryScrape.py
    HIDE PASSWORD for DB

--- Python_Modules/InventoryScrape.py
+++ Python_Modules/InventoryScrape.py
@@ -78,7 +78,7 @@ def write_to_db(zip_codes):
         print("iiiii",  i)
 
     # drop city_zip_data if it exist already- use only when needed. Not needed if you are appending to table.
-    rds_connection_string = "bedlgjelgbrcba:62edbf5e39edf1ea129a38a5766d7354579374a6db487103a421c76fd47d78c3@ec2-184-73-232-93.compute-1.amazonaws.com:5432/dd4i4baf4sjibo"
+    rds_connection_string = "bedlgjelgbrcba:HIDE PASSWORD@ec2-184-73-232-93.compute-1.amazonaws.com:5432/dd4i4baf4sjibo"
     engine = create_engine(f'postgresql://{rds_connection_string}')
     engine.table_names()
     market_scraped_data = 'market_scraped_data'
